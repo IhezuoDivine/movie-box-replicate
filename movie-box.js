1317cd566790e4dcd4ecac31d7dfa4e82743299b
Ihezuo Divine Chikezie <ihezuodivine@gmail.com>
2025-06-12 18:54:36 +0100

    Home page completed

--- movie-box.js
+++ movie-box.js
@@ -1,7 +1,20 @@
-document.addEventListener('DOMContentLoaded', () => {
-  // Wait 3 seconds, then hide splash and show login/signup
+document.addEventListener("DOMContentLoaded", () => {
+  // Wait 3 seconds, then hide splash
   setTimeout(() => {
-    document.getElementById('splash-screen').style.display = 'none';
-    document.getElementById('main').style.display = 'block';
-  }, 3000);
+    document.getElementById("splash-screen").style.display = "none";
+    document.getElementById("main").style.display = "block";
+  }, 2000);
+
+  document.getElementById("search").addEventListener("input", function () {
+    const filter = this.value.toLowerCase();
+    const items = document.querySelectorAll("li, h1, p");
+
+    items.forEach((item) => {
+      if (item.textContent.toLowerCase().includes(filter)) {
+        item.style.display = "";
+      } else {
+        item.style.display = "none";
+      }
+    });
+  });
 });
